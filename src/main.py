1ad25c49a94049093de19ecf67a6b1a224cfd2c6
Kevin Saff <Kevin.Saff@spectrumdsi.com>
2010-06-01 17:48:33 -0500

    More detailed draw routine.

--- src/main.py
+++ src/main.py
@@ -84,15 +84,45 @@ class draw_tool(tool):
         self.location = location
         self.map = map
         self.field = field
+        self.last_point= None
+        
+    def _point(self, event):
+        return [self.location.origin[i] + event.pos[i] for i in (0, 1)]
+    
+    def _map(self, point):
+        return self.map(point, self.field)
+    
+    def _draw(self, point0, point1 = None):
+        if point1 is None or point0 == point1:
+            self.field[self._map(point0)] = 1
+            return
+        
+        diff = [abs(point0[i] - point1[i]) for i in (0, 1)]
+        if diff[0] >= diff[1]:
+            for x in range(point0[0], point1[0] + 1):
+                p = (x - point0[0]) * 1.0 / diff[0]
+                y = int((1 - p) * point0[1] + p * point1[1])
+                self._draw((x, y))
+        else:
+            for y in range(point0[1], point1[1] + 1):
+                p = (y - point0[1]) * 1.0 / diff[1]
+                x = int((1 - p) * point0[0] + p * point1[0])
+                self._draw((x, y))
     
     def handle_event(self, event):
         tool.handle_event(self, event)
         
-        if event.type is pygame.MOUSEBUTTONDOWN and event.button == 1 or \
-                event.type is pygame.MOUSEMOTION and 1 in event.buttons:
-            point = [self.location.origin[i] + event.pos[i] for i in (0,1)]
-            self.field[self.map(point, self.field)] = 1
-        
+        if event.type is pygame.MOUSEBUTTONDOWN and event.button == 1:
+            self.last_point = self._point(event)
+            self._draw(self.last_point)
+        elif self.last_point and event.type is pygame.MOUSEMOTION and 1 in event.buttons:
+            next_point = self._point(event)
+            self._draw(self.last_point, next_point)
+            self.last_point = next_point
+        elif event.type is pygame.MOUSEBUTTONUP and event.button == 1:
+            self._draw(self.last_point, self._point(event))
+            self.last_point = None
+            
     
 
 def main():

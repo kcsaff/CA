958ba2335a95e26164c9153bc58b320b3fc783ff
Kevin Saff <Kevin.Saff@spectrumdsi.com>
2010-06-01 13:44:54 -0500

    One less memory lookup per loop.

--- src/exmod/xxmodule.c
+++ src/exmod/xxmodule.c
@@ -67,8 +67,8 @@ xx_evolve(PyObject *self, PyObject *args)
 	unsigned xstride, ystride;
 	unsigned xa, x0, x1, xM;
 	unsigned ya, y0, y1, yM;
-	unsigned c, z, n;
-	int r;
+	unsigned z;
+	char c, n, r;
 
 	if (!PyArg_ParseTuple(args, "O!O!O!",
 			&PyArray_Type, &input,
@@ -88,21 +88,23 @@ xx_evolve(PyObject *self, PyObject *args)
 	{
 		xa = x0 - xstride;
 		x1 = x0 + xstride;
+		n = input-> data[x0 + 1 * ystride];
 		z = ((input-> data[xa + 0 * ystride] & 1) << 0x3) |
 			((input-> data[x0 + 0 * ystride] & 1) << 0x4) |
 			((input-> data[x1 + 0 * ystride] & 1) << 0x5) |
 			((input-> data[xa + 1 * ystride] & 1) << 0x6) |
-			((input-> data[x0 + 1 * ystride] & 1) << 0x7) |
+			((n & 1) << 0x7) |
 			((input-> data[x1 + 1 * ystride] & 1) << 0x8) ;
 		for (y0 = ystride; y0 < yM; y0 += ystride)
 		{
 			z >>= 3;
 			y1 = y0 + ystride;
+			c = n;
+			n = input-> data[x0 + y1];
 			z |=((input-> data[xa + y1] & 1) << 0x6) |
-				((input-> data[x0 + y1] & 1) << 0x7) |
+				((n & 1) << 0x7) |
 				((input-> data[x1 + y1] & 1) << 0x8) ;
-			c = ( input-> data[x0 + y0] & 0xFE) << 8;
-			r = lookup-> data[z | c];
+			r = lookup-> data[z | ((c & 0xFE) << 8)];
 			output-> data[x0 + y0] = (char)r;
 		}
 	}

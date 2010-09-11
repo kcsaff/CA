4bf95ab3cb2e12bafb10eb5b2c1e817761b230a7
Kevin <kevin@zharus.(none)>
2010-09-11 16:56:02 -0500

    Added some copyright messages.

--- src/window.py
+++ src/window.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2010 by Kevin Saff
+
+# This file is part of the CA scanner.
+
+# The CA scanner is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# The CA scanner is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with the CA scanner.  If not, see <http://www.gnu.org/licenses/>.
+
 from cascading_object import cascading_object
 from run import run
 import worlds, views, display
